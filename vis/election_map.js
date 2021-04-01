71251c23dc659cb81ed358a4069646a98de28292
Eduardo <edumontesuma@gmail.com>
2021-04-01 19:20:41 -0300

    Corrected election map

--- vis/election_map.js
+++ vis/election_map.js
@@ -1,6 +1,6 @@
 const width = 650;
 const height = 480;
-const barplot_width = 600;
+const barplot_width = 700;
 const barplot_height = 150;
 const bolsonaroColor = "#39bd60"
 const haddadColor = "#c43737"
@@ -17,6 +17,36 @@ scaleBolsonaro = d3.scaleQuantize()
 scaleHaddad = d3.scaleQuantize()
                 .domain([0, 1])
                 .range(d3.schemeReds[9]);
+
+const nomeToSigla = {
+    'Acre': 'AC',
+    'Alagoas': 'AL',
+    'Amapá': 'AP',
+    'Amazonas': 'AM',
+    'Bahia': 'BA',
+    'Ceará': 'CE',
+    'Distrito Federal': 'DF',
+    'Espírito Santo': 'ES',
+    'Goiás': 'GO',
+    'Maranhão': 'MA',
+    'Mato Grosso': 'MT',
+    'Mato Grosso do Sul': 'MS',
+    'Minas Gerais': 'MG',
+    'Pará': 'PA',
+    'Paraíba': 'PB',
+    'Paraná': 'PR',
+    'Pernambuco': 'PE',
+    'Piauí': 'PI',
+    'Rio de Janeiro': 'RJ',
+    'Rio Grande do Norte': 'RN',
+    'Rio Grande do Sul': 'RS',
+    'Rondônia': 'RO',
+    'Roraima': 'RR',
+    'Santa Catarina': 'SC',
+    'São Paulo': 'SP',
+    'Sergipe': 'SE',
+    'Tocantins': 'TO'
+}
 
 
 let promises = [
@@ -122,36 +152,42 @@ function ready(us){
             });
 
     // Facts and dimensions about municípios
-    let aux_resultadoMunicipio = []
-    resultadoMunicipio.forEach(function(d) {
+    aux_resultadoMunicipio = []
+    resultadoMunicipio.forEach(function(d, i) {
         aux_resultadoMunicipio.push({
+            "index": i,
+            "uf": d.uf,
             "nome": d.uf + "," + d.nome,
             "totalVotos": d.totalVotos,
             "percentHaddad": d.percentHaddad,
             "percentBolsonaro": d.percentBolsonaro
         })
     })
-    facts2 = crossfilter(aux_resultadoMunicipio)
-    municipioDimension = facts2.dimension(d => d['nome'])
-    numeroVotosDimension = facts2.dimension(d => d['totalVotos'])
-    console.log(numeroVotosDimension.top(5))
-    let initialFilter = []
-    numeroVotosDimension.top(5).forEach(function(d) {
-        initialFilter.push(d.nome)
+    tmp_facts = crossfilter(aux_resultadoMunicipio)
+    allMunicipioDimension = tmp_facts.dimension(d => d['nome'])
+    numeroVotosDimension = tmp_facts.dimension(d => d['totalVotos'])
+    let initialData = []
+    numeroVotosDimension.top(5).forEach(function(d, i) {
+        initialData.push({
+            "index": i,
+            "nome": d.nome,
+            "totalVotos": d.totalVotos,
+            "percentHaddad": d.percentHaddad,
+            "percentBolsonaro": d.percentBolsonaro
+        })
     })
 
+    facts2 = crossfilter(initialData)
+    municipioDimension = facts2.dimension(d => d['nome'])
     municipioVotosHaddad = municipioDimension.group().reduceSum(function(d) {return d.percentHaddad})
     municipioVotosBolsonaro = municipioDimension.group().reduceSum(function(d) {return d.percentBolsonaro})
-    municipioScale = d3.scaleOrdinal().domain(initialFilter)
-    municipioDimension.filterFunction(function(d) {
-        return initialFilter.indexOf(d) > -1;
-    })
+    municipioScale = d3.scaleOrdinal().domain(municipioDimension)
 
     // dc.js barchart
     let barchart2 = new dc.BarChart("#bar2");
     barchart2.width(barplot_width)
              .height(barplot_height)
-             .gap(100)
+             .gap(50)
              .dimension(municipioDimension)
              .x(municipioScale)
              .xUnits(dc.units.ordinal)
@@ -164,11 +200,12 @@ function ready(us){
                  chart.selectAll("g.stack rect.bar").style('fill', function (d) {
                      if (d.layer == 0) {
                          return haddadColor
-                     }else {
+                     } else {
                          return bolsonaroColor
                      }
                  })
              });
+             
     
     dc.renderAll()
 }
@@ -176,8 +213,63 @@ function ready(us){
 
 function highlightFeature(e) {
     let layer = e.target;
-    console.log(e)
-    //console.log(e.target)
+    let overState = nomeToSigla[layer.feature.properties.name]
+    document.getElementById("barHeader2").innerHTML = `5 Municípios com mais votos: ${layer.feature.properties.name}`
+
+    let allMunicipios_fromState = []
+    aux_resultadoMunicipio.forEach(function(d) {
+        if (d.uf.toUpperCase() == overState) {
+            allMunicipios_fromState.push({
+                "uf": d.uf.toUpperCase(),
+                "nome": d.nome,
+                "totalVotos": d.totalVotos,
+                "percentHaddad": d.percentHaddad,
+                "percentBolsonaro": d.percentBolsonaro            
+            })
+        }
+    })
+
+    localFacts = crossfilter(allMunicipios_fromState)
+    localDim = localFacts.dimension(d => d['totalVotos'])
+
+    let initialData = []
+    localDim.top(5).forEach(function(d, i) {
+        initialData.push({
+            "index": i,
+            "nome": d.nome,
+            "totalVotos": d.totalVotos,
+            "percentHaddad": d.percentHaddad,
+            "percentBolsonaro": d.percentBolsonaro
+        })
+    })
+
+    facts2 = crossfilter(initialData)
+    municipioDimension = facts2.dimension(d => d['nome'])
+    municipioVotosHaddad = municipioDimension.group().reduceSum(function(d) {return d.percentHaddad})
+    municipioVotosBolsonaro = municipioDimension.group().reduceSum(function(d) {return d.percentBolsonaro})
+    municipioScale = d3.scaleOrdinal().domain(municipioDimension)
+    let barchart2 = new dc.BarChart("#bar2");
+    barchart2.width(barplot_width)
+             .height(barplot_height)
+             .gap(50)
+             .dimension(municipioDimension)
+             .x(municipioScale)
+             .xUnits(dc.units.ordinal)
+             .group(municipioVotosHaddad)
+             .stack(municipioVotosBolsonaro)
+             .margins({top: 10, right: 20, bottom: 20, left: 30})
+             .elasticY(true)
+             .renderHorizontalGridLines(true)
+             .on("pretransition", function(chart) {
+                 chart.selectAll("g.stack rect.bar").style('fill', function (d) {
+                     if (d.layer == 0) {
+                         return haddadColor
+                     } else {
+                         return bolsonaroColor
+                     }
+                 })
+             });
+    dc.renderAll();
 
     layer.setStyle({
                 weight: 2,
@@ -189,7 +281,6 @@ function highlightFeature(e) {
     if (!L.Browser.ie && !L.Browser.opera) {
         layer.bringToFront();
     }
-
     // info.update(layer.feature);
 }
 
@@ -200,16 +291,11 @@ function resetHighlight(e) {
 }
 
 
-function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-}
-
-
 function onEachFeature(feature, layer) {
     layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight,
-        click: zoomToFeature
+        // click: zoomToFeature
     });
 }
 
@@ -238,4 +324,8 @@ function getTops(source_group) {
             return source_group.top(5);
         }
     };
+}
+
+function padronizeString(s) {
+    return s.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase()
 }
